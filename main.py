0fee223aba76fdb57b88f38e786894a9042a21af
capehare <cape@haremail.slmail.me>
2025-03-01 13:36:38 +0100

    cleaned up main.py and player.py using containers

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from player import Player
 
 clock = pygame.time.Clock()
 dt = 0
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
 
 def main():
     pygame.init()
@@ -17,9 +20,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatable.update(dt)
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
